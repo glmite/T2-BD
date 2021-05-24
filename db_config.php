8bd764157d41d5a27c770d51301e0d1ba67466b8
glmite <70924917+glmite@users.noreply.github.com>
2021-05-24 17:37:09 -0400

    se agrega el nombre de la base de datos
    falta cambiar la contraseña

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 /* Detalles de la conexión */
-$conn_string = "host=localhost port=5432 dbname=<nombre_db> user=postgres password=<contraseña>";
+$conn_string = "host=localhost port=5432 dbname=tarea-bd user=postgres password=<contraseña>";
 // Recuerde reemplazar "<contraseña>" por su contraseña y "<nombre_db>" por el nombre de su BD. No se incluyen los "<>".
 // Establecemos una conexión con el servidor postgresSQL
 $dbconn = pg_connect($conn_string);
