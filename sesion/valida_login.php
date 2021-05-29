581092b3c136b96f8fba2aaf2c4eaa3c7ad666b5
Bastian951 <70776507+Bastian951@users.noreply.github.com>
2021-05-29 17:26:58 -0400

    Update valida_login.php

--- sesion/valida_login.php
+++ sesion/valida_login.php
@@ -1,3 +1,29 @@
 <?php 
-/* Este archivo debe manejar la lógica de iniciar sesión */
-?>
+include '../db_config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email_usr = $_POST["email"];
+    echo $email_usr;
+    $contrasena = $_POST["pwd"];
+    $query = 'SELECT contraseña FROM usuario WHERE correo = $1';
+    $contrasena_hasheada = pg_query_params($dbconn, $query, array($email_usr));
+    $contrasena_hasheada = pg_fetch_assoc($contrasena_hasheada);
+    $contrasena_hasheada = $contrasena_hasheada["contraseña"];
+
+    echo $contrasena_hasheada;
+
+   
+
+    if(password_verify($contrasena, $contrasena_hasheada)){
+	session_start();
+	$_SESSION["usuario"] = $email_usr;
+	header('Status: 301 Moved Permanently');
+	header('Location: ../index.html');
+        
+
+}
+    else{echo 'Contraseña o email incorrecto intente nuevamente <br>';}
+    
+}
+
+pg_close($dbconn);?>
