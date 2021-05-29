a7cce751a159fcdca5c2915b94327f6b1f2bd6d7
Bastian951 <70776507+Bastian951@users.noreply.github.com>
2021-05-29 17:58:31 -0400

    Update valida_login.php

--- sesion/valida_login.php
+++ sesion/valida_login.php
@@ -1,18 +1,17 @@
 <?php 
 include '../db_config.php';
 
+// Lo siguiente guarda en variables lo que escribio el usuario
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_usr = $_POST["email"];
-    echo $email_usr;
     $contrasena = $_POST["pwd"];
     $query = 'SELECT contraseña FROM usuario WHERE correo = $1';
     $contrasena_hasheada = pg_query_params($dbconn, $query, array($email_usr));
     $contrasena_hasheada = pg_fetch_assoc($contrasena_hasheada);
     $contrasena_hasheada = $contrasena_hasheada["contraseña"];
-
-    echo $contrasena_hasheada;
-
-   
+ 
+//Comprobamos que la contraseña sea correcta. Si la contraseña o el usuario no existe php tirará un warning 
 
     if(password_verify($contrasena, $contrasena_hasheada)){
 	session_start();
