fd6056f7eb012a7380f89b9731ba0e06bc077792
Bastian951 <70776507+Bastian951@users.noreply.github.com>
2021-05-29 17:27:27 -0400

    Update valida_sesion.php

--- sesion/valida_sesion.php
+++ sesion/valida_sesion.php
@@ -1,4 +1,14 @@
-<?php
+<?php 
+session_start();
+
+//En la siguiente linea veremos si el usuario es admin o usuario
+if(isset($_SESSION["usuario"])!=FALSE){
+    $query_type_user = 'SELECT Administrador FROM usuario WHERE correo = $1';
+    $query_type_user  = pg_query_params($dbconn, $query_type_user, array($_SESSION["usuario"]));
+    $query_type_user  = pg_fetch_assoc($query_type_user);
+    $type_user = $query_type_user["administrador"];
+}
+
 /* Este archivo debe usarse para comprobar si existe una sesión válida 
    Considerar qué pasa cuando la sesión es válida/inválida para cada página:
    - Página principal
