17a37623cbc3a00833e39394670ccc5dd0ca0f26
Bastian951 <70776507+Bastian951@users.noreply.github.com>
2021-05-29 17:42:38 -0400

    Update logout.php
    Agregué comentarios

--- sesion/logout.php
+++ sesion/logout.php
@@ -1,4 +1,7 @@
 <?php include 'db_config.php';
+
+//Este php es para cerrar la sesión. Al cerrar la sesión redirecciona a index.html
+
 session_start();   
 session_destroy(); 
 header("location: ../index.html");
